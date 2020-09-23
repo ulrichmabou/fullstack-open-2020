9f6225ad5b1ec34723e87b347d1f7c748093b7ce
Ulrich <ulrichmabou@gmail.com>
2020-09-23 18:10:14 +0100

    Add files via upload

---
+++ notes/part2-4/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import axios from 'axios'
+
+// axios
+// .get('http://localhost:3001/notes')
+// .then(res => {
+//   const notes = res.data
+//   console.log(notes)
+// })
+
+// const promises2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     date: '2019-05-30T17:30:31.098Z',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only Javascript',
+//     date: '2019-05-30T18:39:34.091Z',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     date: '2019-05-30T19:20:14.298Z',
+//     important: true
+//   }
+// ]
+
+// axios
+// .get('http://localhost:3001/notes')
+// .then(res => {
+//   const notes = res.data
+//   ReactDOM.render(
+//     <App notes={notes} />,
+//     document.getElementById('root')
+//   )
+// })
+
+ReactDOM.render(<App />, document.getElementById('root'))
